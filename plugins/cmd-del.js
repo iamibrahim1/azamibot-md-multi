a0bf1cd94d652afb01e1aab1cfe509949d740e41
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-01-31 20:49:11 +0500

    Update cmd-del.js

--- plugins/cmd-del.js
+++ plugins/cmd-del.js
@@ -5,7 +5,7 @@ let handler = async (m, { text }) => {
 	if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
 	if (!hash) throw `Tidak ada hash`
 	let sticker = db.data.sticker
-	if (sticker[hash] && sticker[hash].locked) throw 'Kamu tidak memiliki izin untuk menghapus perintah stiker ini'
+	if (sticker[hash] && sticker[hash].locked) throw 'You do not have permission to remove this sticker prompt'
 	delete sticker[hash]
 	m.reply(`Berhasil!`)
 }
