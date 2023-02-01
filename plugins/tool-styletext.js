ddfd024618bad06e055d4076c17c3c0ea0d1bec2
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-01 15:54:13 +0500

    Update tool-styletext.js

--- plugins/tool-styletext.js
+++ plugins/tool-styletext.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 import { JSDOM } from 'jsdom'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	if (!text) throw `Example: *${usedPrefix + command} nama gwejh*`
+	if (!text) throw `Example: *${usedPrefix + command} Ibrahim*`
 	conn.reply(m.chat, Object.entries(await stylizeText(text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text)).map(([name, value]) => `*${name}*\n${value}`).join`\n\n`, m)
 }
 
