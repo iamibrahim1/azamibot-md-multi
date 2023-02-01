05ba4707b38a4e12bfaa72724980e106d759799e
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-01 15:53:25 +0500

    Update tool-tolatin.js

--- plugins/tool-tolatin.js
+++ plugins/tool-tolatin.js
@@ -1,13 +1,13 @@
 import { aksaraToLatin } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	if (!text) throw `Ubah Aksara Jawa ke Latin\n\nContoh :\n*${usedPrefix + command} ꦲꦭ꧀ꦭꦺꦴ​ꦫꦺꦏ꧀*`
+	if (!text) throw `Change Javanese script to Latin\n\nExample:\n*${usedPrefix + command} ꦲꦭ꧀ꦭꦺꦴ​ꦫꦺꦏ꧀*`
 	try {
 		let anu = await aksaraToLatin(`${text}`)
-		m.reply(`*Hasil :*\n${anu}`)
+		m.reply(`*Result :*\n${anu}`)
 	} catch (e) {
 		console.log(e)
-		m.reply(`Terjadi kesalahan, coba lagi nanti.`)
+		m.reply(`An error occurred, try again later.`)
 	}
 }
 
