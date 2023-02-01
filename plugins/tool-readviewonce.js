91fdfaeeb88e1429f2863b6ccf87878364819dc5
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-01 15:55:56 +0500

    Update tool-readviewonce.js

--- plugins/tool-readviewonce.js
+++ plugins/tool-readviewonce.js
@@ -1,8 +1,8 @@
 import { downloadContentFromMessage } from '@adiwajshing/baileys'
 
 let handler = async (m, { conn }) => {
-	if (!m.quoted) throw 'Quote pesan viewOnce'
-	if (m.quoted.mtype != 'viewOnceMessageV2') throw 'Itu bukan pesan viewOnce'
+	if (!m.quoted) throw 'ViewOnce message quote'
+	if (m.quoted.mtype != 'viewOnceMessageV2') throw 'Its not a viewOnce message'
 	let txt = (m.quoted.message['imageMessage'] ? m.quoted.message.imageMessage.caption : m.quoted.message.videoMessage.caption) || ''
 	let buffer = await m.quoted.download()
 	let j = [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')
